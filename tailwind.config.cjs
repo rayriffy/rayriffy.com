f6e101e858545209534e914f9dad9fa5d99ceac5
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-06 08:19:33 +0900

    refactor: stablized

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: theme => ({
+        h1: {
+          fontWeight: '700',
+          letterSpacing: theme('letterSpacing.tight'),
+          color: theme('colors.gray.900'),
+        },
+        h2: {
+          fontWeight: '700',
+          letterSpacing: theme('letterSpacing.tight'),
+          color: theme('colors.gray.900'),
+        },
+        h3: {
+          fontWeight: '600',
+          color: theme('colors.gray.900'),
+        },
+        h4: {
+          fontSize: '1.166667em',
+        },
+        'h4,h5,h6': {
+          color: theme('colors.gray.900'),
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
