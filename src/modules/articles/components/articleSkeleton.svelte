3c7e5813043483241f054e2ef98f6495523a0b06
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-07 03:16:07 +0900

    chore: staged changes

--- src/modules/articles/components/articleSkeleton.svelte
+++
@@ -1,9 +0,0 @@
-<div class="overflow-hidden rounded-xl border bg-white shadow-md">
-  <div
-    class="aspect-[1500/788] w-full animate-pulse rounded-t-xl bg-gray-700"
-  />
-  <div class="px-6 py-4">
-    <div class="h-6 w-40 animate-pulse rounded-md bg-gray-700" />
-    <div class="mt-2 h-4 w-64 animate-pulse rounded-md bg-gray-700" />
-  </div>
-</div>
