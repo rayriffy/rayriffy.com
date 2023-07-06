06e30702c87de38b3abdfd0a3dd05b215f9b4be0
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-06 18:12:41 +0900

    refactor: code highlighting

--- src/modules/blog/plugins/iframeParser.ts
+++ src/modules/blog/plugins/iframeParser.ts
@@ -63,7 +63,11 @@ export const iframeParser: Plugin = () => {
         if (o.ok) return o.json()
         else throw o
       })
-      await writeFileSystem(oembedKey, providersRemote, 1000 * 60 * 60 * 24 * 30)
+      await writeFileSystem(
+        oembedKey,
+        providersRemote,
+        1000 * 60 * 60 * 24 * 30
+      )
     }
 
     // transform
@@ -128,7 +132,11 @@ export const iframeParser: Plugin = () => {
                       else throw o
                     })
 
-                    writeFileSystem(oembedEndpointKey, oembedResult, 1000 * 60 * 60)
+                    writeFileSystem(
+                      oembedEndpointKey,
+                      oembedResult,
+                      1000 * 60 * 60
+                    )
 
                     // override node
                     node.type = `html`
