d883a37147e5fa942fb7abfad4e44d40b416080b
Phumrapee Limpianchop <git@rayriffy.com>
2025-02-16 05:52:01 +0700

    feat: new hero image

--- src/modules/hero/constants/index.ts
+++ src/modules/hero/constants/index.ts
@@ -2,6 +2,15 @@ import type { HeroItem } from '../@types/HeroItem'
 
 export const heroItems: HeroItem[] = [
   {
+    src: '/front/4.jpg',
+    date: new Date('2024-12-23'),
+    camera: {
+      manufacturer: 'Apple',
+      model: 'iPhone 14 Pro',
+      lens: 'Main Camera — 24 mm ƒ1.78',
+    },
+  },
+  {
     src: '/front/3.jpg',
     date: new Date('2024-02-26'),
     camera: {
